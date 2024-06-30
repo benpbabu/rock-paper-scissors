96ab5202f862890eaa222a57b708f8562fec8cb0
Ben P Babu <benpbabu@gmail.com>
2024-06-30 22:31:10 +0530

    add comments to backbone.py

--- backbone.py
+++ backbone.py
@@ -8,6 +8,8 @@ class RPSBackBone:
 
     def __init__(self) -> None:
         self.choices = ("Rock", "Paper", "Scissors")
+        # Dictionary to compute outcome. Outer key is computer's choice,
+        # inner key is human's choice, inner value is outcome
         self.outcome = {
             "Rock": {"Rock": "Tied", "Paper": "Won", "Scissors": "Failed"},
             "Paper": {"Rock": "Failed", "Paper": "Tied", "Scissors": "Won"},
@@ -17,13 +19,16 @@ class RPSBackBone:
         self.human_choice = None
 
     def choose_for_computer(self) -> None:
+        """Randomly chooses Rock/Paper/Scissors for computer"""
         self.computer_choice = random.choice(self.choices)
         return self.computer_choice
 
     def human_chooses(self, human_choice: str = None) -> None:
+        """Registers human's choice"""
         self.human_choice = human_choice
 
     def result(self) -> str:
+        """Returns outcome & choices"""
         return (
             self.outcome.get(self.computer_choice).get(self.human_choice),
             self.computer_choice,
