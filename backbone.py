38a256a1f938fb6a80790036a05722824cc432fc
Ben P Babu <benpbabu@gmail.com>
2024-06-30 22:25:05 +0530

    add backbone.py with rock-paper-scissors

---
+++ backbone.py
@@ -0,0 +1,44 @@
+import random
+import logging
+from tkinter import Tk, Label, Button, Entry
+
+
+class RPSBackBone:
+    """Class to implement rock paper scissors logic"""
+
+    def __init__(self) -> None:
+        self.choices = ("Rock", "Paper", "Scissors")
+        self.outcome = {
+            "Rock": {"Rock": "Tied", "Paper": "Won", "Scissors": "Failed"},
+            "Paper": {"Rock": "Failed", "Paper": "Tied", "Scissors": "Won"},
+            "Scissors": {"Rock": "Won", "Paper": "Failed", "Scissors": "Tied"},
+        }
+        self.computer_choice = None
+        self.human_choice = None
+
+    def choose_for_computer(self) -> None:
+        self.computer_choice = random.choice(self.choices)
+        return self.computer_choice
+
+    def human_chooses(self, human_choice: str = None) -> None:
+        self.human_choice = human_choice
+
+    def result(self) -> str:
+        return (
+            self.outcome.get(self.computer_choice).get(self.human_choice),
+            self.computer_choice,
+            self.human_choice,
+        )
+
+
+if __name__ == "__main__":
+    rps = RPSBackBone()
+    while True:
+        rps.choose_for_computer()
+        choice = input("Rock, Paper, Scissors: ")
+        if choice in rps.choices:
+            rps.human_chooses(choice)
+            result, c_choice, h_choice = rps.result()
+            print(f"Computer: {c_choice}, You: {h_choice}, Result: You {result}")
+        else:
+            break
