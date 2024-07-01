1445abb4a0ccb3d5d5fe74681bd8c9f256e821fd
Ben P Babu <benpbabu@gmail.com>
2024-07-01 18:49:13 +0530

    conncect backbone logic to gui frontend

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import logging
 from tkinter import Tk, Label, Button, Entry
 import time
+from backbone import RPSBackBone
 
 
 class RockPaperScissorsApp:
@@ -11,15 +12,24 @@ class RockPaperScissorsApp:
         self.root_window = Tk()
         self.create_widgets()
         self.place_widgets()
+        self.rock_paper_scissors = RPSBackBone()
 
     def create_widgets(self) -> None:
         """Function to create widgets in root window"""
         self.computer_choice_label = Label(self.root_window, text="Rock")
         self.human_choice_label = Label(self.root_window, text="Rock")
         self.outcome_label = Label(self.root_window, text="Outcome")
-        self.rock_button = Button(self.root_window, text="Rock")
-        self.paper_button = Button(self.root_window, text="Paper")
-        self.scissors_button = Button(self.root_window, text="Scissors")
+        self.rock_button = Button(
+            self.root_window, text="Rock", command=lambda: self.user_plays("Rock")
+        )
+        self.paper_button = Button(
+            self.root_window, text="Paper", command=lambda: self.user_plays("Paper")
+        )
+        self.scissors_button = Button(
+            self.root_window,
+            text="Scissors",
+            command=lambda: self.user_plays("Scissors"),
+        )
         self.exit_button = Button(self.root_window, text="Exit", command=self.close)
 
     def place_widgets(self) -> None:
@@ -31,6 +41,16 @@ class RockPaperScissorsApp:
         self.paper_button.grid(row=1, column=1)
         self.scissors_button.grid(row=1, column=2)
         self.exit_button.grid(row=2, column=2)
+
+    def user_plays(self, user_choice: str) -> None:
+        """Callback function when user clicks a button (Rock/Paper/Scissors)"""
+        self.rock_paper_scissors.choose_for_computer()
+        self.rock_paper_scissors.human_chooses(user_choice)
+        result, c_choice, h_choice = self.rock_paper_scissors.result()
+        outcome = f"You {result}"
+        self.computer_choice_label.config(text=c_choice)
+        self.human_choice_label.config(text=h_choice)
+        self.outcome_label.config(text=outcome)
 
     def run(self) -> None:
         self.root_window.mainloop()
